fafa88c8f7c336ada009d5bbe41b1361354a122f
bianKOPI <46771168+bianKOPI@users.noreply.github.com>
2019-02-06 08:49:37 +0700

    Add files via upload

---
+++ kotak.js
@@ -0,0 +1,4 @@
+<script type='text/javascript'>
+var pres = document.getElementsByTagName(&quot;pre&quot;);for (var i = 0; i &lt; pres.length; i++) {pres[i].addEventListener(&quot;dblclick&quot;, function () {
+var selection = getSelection();var range = document.createRange();range.selectNodeContents(this);selection.removeAllRanges();selection.addRange(range);}, false);}
+</script>
